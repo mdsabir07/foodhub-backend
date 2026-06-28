1759d674ee75db46ca91c23b436a212c76099218
Sabirul Islam <45106497+mdsabir07@users.noreply.github.com>
2026-06-28 13:55:24 +0600

    Created server.ts, app.ts and src/lib/prisma.ts and database and server run successfully

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const port = process.env.PORT || 3000;
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully.");
+
+        app.listen(port, () => {
+            console.log(`The server is running on port http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error("Error in main function: ", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+
+main();
